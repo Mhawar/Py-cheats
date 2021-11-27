d3dfabab3daf3104f656e402326a5f9954522d98
Mhawar <Mhawar2018a@gmail.com>
2021-11-27 19:56:55 +0100

    add a loop + function

---
+++ pythonoperations.py
@@ -0,0 +1,41 @@
+
+
+
+def principles():
+#Math Operators#
+
+ aa = int(1)
+ bb = int(2)
+ #Expo
+ a= aa ** bb
+ #Modulus/Remainder
+ b= aa % bb
+ #Integer division
+ c= aa // bb
+ #Division
+ d= aa / bb
+ #Multiplication
+ e= aa * bb
+ #Subtraction
+ f= aa - bb
+ #Addition
+ g= aa + bb 
+
+ Variable = input()
+ if Variable == 'expo' :
+     print('a ** b =',a)
+ elif Variable == 'modulus' :
+     print('a/b reste  =',b)
+ elif Variable == 'int division' :
+     print('a // b =',c)
+ elif Variable == 'division' : print('a / b =',d)
+ elif Variable == 'multiplication' : print('a * b =',e)
+ elif Variable == 'substraction' : print('a - b =',f)
+ elif Variable == 'addition' : print('a + b =',g)
+
+ 
+
+
+
+
+
